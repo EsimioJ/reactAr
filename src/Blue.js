4f1b08bdfdbf641a1dcbd45807547833c31ebe63
Jacopo Riccioni <EsimioJ@users.noreply.github.com>
2023-04-05 11:25:29 +0200

    backup blue - AR not working

--- src/Blue.js
+++ src/Blue.js
@@ -7,21 +7,22 @@ import { useStore } from "./hooks";
 // import Shaco from "./model/wooden_table.glb";
 //import Shaco from "./model/testBike2.gltf";
 import Shaco from "./model/Sondrio1DEF.gltf";
+
+
 export default function Blue() {
+  const urlModel = useStore((state) => state.modelState);
   return (
     <div>
+      TEST BBLUE
+      <NavLink to="/render"> RENDER </NavLink>
       <model-viewer
         id="test"
         ar
-        camera-orbit="10deg 75deg" 
-        scale="0.4 0.4 0.4"
-        background-color="#622dcf"
         autoplay
         ar-modes="webxr scene-viewer quick-look"
         camera-controls
-        camera-target="1m 0m 0m" 
         src={Shaco}
-        alt="Benvenuto nella via dei palazzi di Sondrio"
+        alt="A 3D shoes"
       >
         <button
           slot="ar-button"
@@ -31,8 +32,8 @@ export default function Blue() {
             borderRadius: "4px",
             border: "none",
             position: "absolute",
-            bottom: "16px",
-            right: "30%"
+            top: "16px",
+            right: "16px"
           }}
         >
           Activate AR
@@ -41,3 +42,40 @@ export default function Blue() {
     </div>
   );
 }
+
+
+
+// export default function Blue() {
+//   return (
+//     <div>
+//       <model-viewer
+//         id="test"
+//         ar
+//         camera-orbit="10deg 75deg" 
+//         scale="0.4 0.4 0.4"
+//         background-color="#622dcf"
+//         autoplay
+//         ar-modes="webxr scene-viewer quick-look"
+//         camera-controls
+//         camera-target="1m 0m 0m" 
+//         src={Shaco}
+//         alt="Benvenuto nella via dei palazzi di Sondrio"
+//       >
+//         <button
+//           slot="ar-button"
+//           style={{
+//             backgroundColor: "black",
+//             color: "white",
+//             borderRadius: "4px",
+//             border: "none",
+//             position: "absolute",
+//             bottom: "16px",
+//             right: "30%"
+//           }}
+//         >
+//           Activate AR
+//         </button>
+//       </model-viewer>
+//     </div>
+//   );
+// }
